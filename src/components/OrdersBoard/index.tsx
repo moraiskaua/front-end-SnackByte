fed8a11323cbd24c2d843ec58bcd1b061b019eff
moraiskaua <moraiskaua03@gmail.com>
2024-02-17 02:15:20 -0300

    feat: create board component

---
+++ src/components/OrdersBoard/index.tsx
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+import { OrderType } from '../../types/Order';
+import { Board, OrdersContainer } from './styles';
+import OrderModal from '../OrderModal';
+
+interface OrdersBoardProps {
+  icon: string;
+  title: string;
+  orders: OrderType[];
+}
+
+const OrdersBoard: React.FC<OrdersBoardProps> = ({ icon, title, orders }) => {
+  const [selectedOrder, setSelectedOrder] = useState<OrderType | null>(null);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const handleOpenModal = (order: OrderType) => {
+    setIsModalVisible(true);
+    setSelectedOrder(order);
+  };
+
+  return (
+    <Board>
+      <OrderModal visible={isModalVisible} order={selectedOrder} />
+
+      <header>
+        <span>{icon}</span>
+        <strong>{title}</strong>
+        <span>({orders.length})</span>
+      </header>
+
+      {orders.length > 0 && (
+        <OrdersContainer>
+          {orders.map(order => (
+            <button
+              key={order._id}
+              type="button"
+              onClick={() => handleOpenModal(order)}
+            >
+              <strong>Mesa {order.table}</strong>
+              <span>{order.products.length} itens</span>
+            </button>
+          ))}
+        </OrdersContainer>
+      )}
+    </Board>
+  );
+};
+
+export default OrdersBoard;
